05f6d0f09f560f2d61b2f7ce2e45b3c7f7c3fc4e
“Sabrina <angellerinac@gmail.com>
2022-06-22 14:59:57 -0400

    reset spotify uri

--- frontend/src/contexts/TokenContext.js
+++ frontend/src/contexts/TokenContext.js
@@ -4,7 +4,8 @@ export const TokenContext = createContext(null);
 
 export const TokenProvider = ({ children }) => {
   // Authorization params
-  const REDIRECT_URI = "http://localhost:3000/callback";
+  const REDIRECT_URI =
+    "https://netlify-thinks-angellerina-is-great.netlify.app/";
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
 
